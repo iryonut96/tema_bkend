cfc628a168a05db61ce455f245e7dc95e286d92f
iryonut96 <irinel.ionut96@yahoo.com>
2017-06-12 10:55:47 +0300

    Login and Post Fixed
    You can now add articles and go to the Homepage via "Home"

--- test/tema/flaskr.py
+++ test/tema/flaskr.py
@@ -54,21 +54,28 @@ def initdb_command():
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
+    cur = db.execute('select title, text, minitext from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
+@app.route('/index')
+def home():
+    db = get_db()
+    cur = db.execute('select title, text, minitext from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('test.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    db.execute('insert into entries (title, text, minitext) values (?, ?, ?)',
+                 [request.form['title'], request.form['text'], request.form['minitext']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
 
 # Login and Logout
 @app.route('/login', methods=['GET', 'POST'])
